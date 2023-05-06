0f8dc048b00af957a21c28474530bb2d24b594fa
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-05-06 21:48:38 +0300

    add author entity type configuration

---
+++ src/EfManyToManySample/AuthorEntityTypeConfiguration.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace EfManyToManySample
+{
+  using Microsoft.EntityFrameworkCore;
+  using Microsoft.EntityFrameworkCore.Metadata.Builders;
+  using Microsoft.EntityFrameworkCore.ValueGeneration;
+
+  /// <summary>Defines an entity type configuration for the <see cref="EfManyToManySample.AuthorEntity"/>.</summary>
+  public sealed class AuthorEntityTypeConfiguration : IEntityTypeConfiguration<AuthorEntity>
+  {
+    /// <summary>Configures the entity of type <see cref="EfManyToManySample.AuthorEntity"/>.</summary>
+    /// <param name="builder">An object that provides a simple API for configuring an <see cref="Microsoft.EntityFrameworkCore.Metadata.IMutableEntityType" />.</param>
+    public void Configure(EntityTypeBuilder<AuthorEntity> builder)
+    {
+      builder.ToTable("author");
+      builder.HasKey(entity => entity.AuthorId);
+
+      builder.Property(entity => entity.AuthorId)
+             .HasColumnName("id")
+             .IsRequired()
+             .ValueGeneratedOnAdd()
+             .HasValueGenerator<GuidValueGenerator>();
+    }
+  }
+}
