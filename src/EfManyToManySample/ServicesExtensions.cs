c381c72dba39d9796a2b915d7b522ecd9a9fb4c4
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-05-06 22:01:35 +0300

    add services extensions

---
+++ src/EfManyToManySample/ServicesExtensions.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+  using Microsoft.EntityFrameworkCore;
+  using Microsoft.Extensions.Options;
+
+  using EfManyToManySample;
+
+  /// <summary>Extends an API of the <see cref="Microsoft.Extensions.DependencyInjection.IServiceCollection"/>.</summary>
+  public static class ServicesExtensions
+  {
+    /// <summary>Sets up the databse.</summary>
+    /// <param name="services">An object that specifies the contract for a collection of service descriptors.</param>
+    /// <returns>An object that specifies the contract for a collection of service descriptors.</returns>
+    public static IServiceCollection SetUpDatabase(this IServiceCollection services)
+    {
+      services.AddDbContext<DbContext, AppDbContext>((provider, builder) =>
+      {
+        var options = provider.GetRequiredService<IOptions<DatabaseOptions>>().Value;
+
+        if (string.IsNullOrWhiteSpace(options.ConnectionString))
+        {
+          throw new ArgumentNullException(nameof(DatabaseOptions.ConnectionString));
+        }
+
+        builder.UseNpgsql(options.ConnectionString);
+      });
+
+      return services;
+    }
+  }
+}
