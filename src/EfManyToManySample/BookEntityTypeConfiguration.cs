df60a2551c2bb699fecadf2491face7a9d2115f9
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-05-06 21:51:28 +0300

    add book entity type configuration

--- src/EfManyToManySample/BookEntityTypeConfiguration.cs
+++ src/EfManyToManySample/BookEntityTypeConfiguration.cs
@@ -8,39 +8,39 @@ namespace EfManyToManySample
   using Microsoft.EntityFrameworkCore.Metadata.Builders;
   using Microsoft.EntityFrameworkCore.ValueGeneration;
 
-  /// <summary>Defines an entity type configuration for the <see cref="EfManyToManySample.AuthorEntity"/>.</summary>
-  public sealed class AuthorEntityTypeConfiguration : IEntityTypeConfiguration<AuthorEntity>
+  /// <summary>Defines an entity type configuration for the <see cref="EfManyToManySample.BookEntity"/>.</summary>
+  public sealed class BookEntityTypeConfiguration : IEntityTypeConfiguration<BookEntity>
   {
-    /// <summary>Configures the entity of type <see cref="EfManyToManySample.AuthorEntity"/>.</summary>
+    /// <summary>Configures the entity of type <see cref="EfManyToManySample.BookEntity"/>.</summary>
     /// <param name="builder">An object that provides a simple API for configuring an <see cref="Microsoft.EntityFrameworkCore.Metadata.IMutableEntityType" />.</param>
-    public void Configure(EntityTypeBuilder<AuthorEntity> builder)
+    public void Configure(EntityTypeBuilder<BookEntity> builder)
     {
-      builder.ToTable("author");
-      builder.HasKey(entity => entity.AuthorId);
+      builder.ToTable("book");
+      builder.HasKey(entity => entity.BookId);
 
-      builder.Property(entity => entity.AuthorId)
+      builder.Property(entity => entity.BookId)
              .HasColumnName("id")
              .IsRequired()
              .ValueGeneratedOnAdd()
              .HasValueGenerator<GuidValueGenerator>();
 
-      builder.Property(entity => entity.Name)
-             .HasColumnName("name")
+      builder.Property(entity => entity.Title)
+             .HasColumnName("title")
              .IsRequired()
              .HasMaxLength(256);
 
-      builder.HasMany(entity => entity.Books)
-             .WithMany(entity => entity.Authors)
+      builder.HasMany(entity => entity.Authors)
+             .WithMany(entity => entity.Books)
              .UsingEntity<BookAuthorEntity>(
                "book_author",
-               builder => builder.HasOne(entity => entity.Book)
-                                 .WithMany()
-                                 .HasForeignKey(entity => entity.BookId)
-                                 .HasPrincipalKey(entity => entity.BookId),
                builder => builder.HasOne(entity => entity.Author)
                                  .WithMany()
                                  .HasForeignKey(entity => entity.AuthorId)
-                                 .HasPrincipalKey(entity => entity.AuthorId)
+                                 .HasPrincipalKey(entity => entity.AuthorId),
+               builder => builder.HasOne(entity => entity.Book)
+                                 .WithMany()
+                                 .HasForeignKey(entity => entity.BookId)
+                                 .HasPrincipalKey(entity => entity.BookId)
              );
     }
   }
