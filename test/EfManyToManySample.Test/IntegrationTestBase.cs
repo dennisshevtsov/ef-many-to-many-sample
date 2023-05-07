5f5edb9b968102793e957bbbd67c4efc9920cc2e
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-05-07 19:22:38 +0300

    add integration test base

---
+++ test/EfManyToManySample.Test/IntegrationTestBase.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace EfManyToManySample.Test
+{
+  using Microsoft.EntityFrameworkCore;
+  using Microsoft.Extensions.DependencyInjection;
+
+  [TestClass]
+  public abstract class IntegrationTestBase
+  {
+#pragma warning disable CS8618
+    private IServiceScope _scope;
+
+    protected DbContext DbContext { get; private set; }
+#pragma warning restore CS8618
+
+    [TestInitialize]
+    public void Initialize()
+    {
+      var scope = new ServiceCollection().SetUpDatabase()
+                                         .BuildServiceProvider()
+                                         .CreateScope();
+
+      DbContext = scope.ServiceProvider.GetRequiredService<DbContext>();
+      _scope = scope;
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+      DbContext?.Database.EnsureDeleted();
+      _scope?.Dispose();
+    }
+  }
+}
